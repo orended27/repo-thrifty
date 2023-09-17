2bc70ebf5cd6ce98efc620274400e477efe969fb
orended27 <130698429+orended27@users.noreply.github.com>
2023-09-17 21:24:36 +0300

    Trigger rebuild

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // import './App.css'; 
 // import NavBar from './components/NavBar/index';
-// import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './pages/Layout';
 import Home from './pages/Home';
